8a783678a491bc71e371ab144e9473c5a68a7368
iopopoi <kimkin0668@gmail.com>
2021-11-04 21:31:10 +0900

    Algorithm repository, first Q : 1389. 촌수계산

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # algorithm
+
+백준문제 풀이
+
+## graph
+그래프 탐색과 관련된 문제와 풀이 코드
+1. graph
+2. dfs
+3. bfs
+4. tree
+5. etc...
---
+++ graph/탐색/1389.cpp
@@ -0,0 +1,46 @@
+/*
+    백준 1389번 - 촌수계산
+    두 사람 사이의 촌수를 계산하는 문제이다.
+    분야 - graph, dfs, bfs
+*/
+#include <bits/stdc++.h>
+#define mp make_pair
+#define pb push_back
+#define fst first
+#define snd second
+
+using namespace std;
+
+int N, S, F;
+int chk[101];
+vector<vector<int>> tree(101);
+
+int order(){
+    queue < pair<int,int> > q; // index, time
+    q.push({S,0});
+    chk[S] = 1;
+    while(!q.empty()){
+        int index = q.front().fst, time = q.front().snd;
+        q.pop();
+        for(int i=0;i<tree[index].size();i++){
+            int next = tree[index][i];
+            if(next == F) return time + 1;
+            if(chk[next]==0){
+                q.push({next, time+1 });
+                chk[next] = 1;
+            }
+        }
+    }
+    return -1;
+}
+
+int main(){
+    int edge, p,c;
+    cin >> N >> S >> F >> edge;
+    while(edge--){
+        cin >> p >> c;
+        tree[p].pb(c);
+        tree[c].pb(p);
+    }
+    cout << order();
+}
