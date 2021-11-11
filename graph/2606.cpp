654d5da56a386b24f93e2cc76cd1ee63d083367c
iopopoi <kimkin0668@gmail.com>
2021-11-11 21:49:11 +0900

    ADD: 2606 틀

---
+++ graph/2606.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+#define mp make_pair
+#define pb push_back
+#define fst first
+#define snd second
+
+using namespace std;
+
+int N, E;
+vector<vector<int>> v(101); // v[i] : i번째 노드와 연결된 노드의 정보
+
+int main(){
+    cin >> N >> E;
+    while (E--)
+    {
+        int a, b; cin >> a >> b;
+
+    }
+    
+}
