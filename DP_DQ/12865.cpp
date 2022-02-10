0e4de6b1f8cda3bdcdcd3b0f8b6eb6aae197dace
iopopoi <kimkin0668@gmail.com>
2022-02-11 01:33:28 +0900

    ADD: algorithm: DP와 분할정복

---
+++ DP_DQ/12865.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+
+using namespace std;
+
+int N, K;
+int dp[101][100001]; // i ~ N까지 물건을 들때 최대 무게, 가치
+int arr[101][2]; // i번 물건의 무게, 가치
+
+// n ~ N까지 k의 무게로 들수 있는 max값 반환
+int dfs(int n, int k){
+    if(n == N){
+        if(arr[N][0] <= k) return dp[n][k] = arr[n][1];
+        else return dp[n][k] = 0;
+    }
+    if(dp[n][k] != -1 ) return dp[n][k];
+    
+    if(arr[n][0] <= k) return dp[n][k] = max(dfs(n+1,k), dfs(n+1, k - arr[n][0]) + arr[n][1] );
+    else return dp[n][k] = dfs(n+1, k);
+}
+
+int main(){
+    cin.tie(0); ios_base::sync_with_stdio(0);
+    cin >> N >> K;
+    for(int i=1;i<=N;i++){
+        cin >> arr[i][0] >> arr[i][1];
+    }
+
+    memset(dp, -1,sizeof(dp));
+
+    // dfs(i, K) : i ~ N의 조건을 충족하는 값 반환 , 남은 무게
+    cout << dfs(1, K); 
+}
